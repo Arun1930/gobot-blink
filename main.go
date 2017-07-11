f28f31c4d99584e8f229dc159e8a95b32eb711e4
Leo Arias <leo.arias@canonical.com>
2017-07-11 05:45:13 +0000

    Support dragonboard and beaglebone

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+  "log"
+  "os/exec"
+  "strings"
   "time"
 
   "gobot.io/x/gobot"
   "gobot.io/x/gobot/drivers/gpio"
+  "gobot.io/x/gobot/platforms/beaglebone"
+  "gobot.io/x/gobot/platforms/dragonboard"
   "gobot.io/x/gobot/platforms/raspi"
 )
 
 func main() {
-  adaptor := raspi.NewAdaptor()
-  led := gpio.NewLedDriver(adaptor, "7")
+  out, err := exec.Command("uname", "-r").Output()
+  if err != nil {
+    log.Fatal(err)
+  }
+  var adaptor gobot.Adaptor
+  var pin string
+  kernelRelease := string(out)
+  if strings.Contains(kernelRelease, "raspi2") {
+    adaptor = raspi.NewAdaptor()
+    pin = "7"
+  } else if strings.Contains(kernelRelease, "snapdragon") {
+    adaptor = dragonboard.NewAdaptor()
+    pin = "GPIO_A"
+  } else {
+    adaptor = beaglebone.NewAdaptor()
+    pin = "P8_7"
+  }
+  digitalWriter, ok := adaptor.(gpio.DigitalWriter)
+  if !ok {
+    log.Fatal("Invalid adaptor")
+  }
+  led := gpio.NewLedDriver(digitalWriter, pin)
 
   work := func() {
     gobot.Every(1*time.Second, func() {
